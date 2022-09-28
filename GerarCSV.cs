d1b366bed7e66a3d7e939c4eefc0b2e3ea736152
Dr141 <rhemersondr14@gmail.com>
2022-09-28 09:14:52 -0300

    Adicionar arquivos de projeto.

---
+++ GerarCSV.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace E_log
+{
+    class GerarCSV
+    {
+        public string nomeArquivo { get; private set; }
+
+        public bool InicaArquivo()
+        {
+            this.nomeArquivo = "Dia_" + DateTime.Now.ToString().Remove(10).Replace("/", "") + ".CSV";
+
+            try
+            {
+                if (!File.Exists(this.nomeArquivo))
+                {
+                    FileStream file = new FileStream(nomeArquivo, FileMode.Create);
+                    file.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public void EscreverCSV(List<string> mensagem)
+        {
+            if (this.InicaArquivo())
+            {
+                FileStream file = new FileStream(nomeArquivo, FileMode.Append);
+                StreamWriter writer = new StreamWriter(file);
+
+                foreach (var men in mensagem)
+                {
+                    writer.WriteLine(men);
+                }                
+                
+                writer.Close();
+                file.Close();
+            }
+        }
+    }
+}
---
+++ GerarLog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace E_log
+{
+    public class GerarLog
+    {
+        public string nomeArquivo { get; private set; }
+
+        public bool InicaArquivo()
+        {
+            this.nomeArquivo = "Dia_" + DateTime.Now.ToString().Remove(10).Replace("/", "") + ".log";
+
+            try
+            {
+                if (!File.Exists(this.nomeArquivo))
+                {
+                    FileStream file = new FileStream(nomeArquivo, FileMode.Create);
+                    file.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public void EscreverLog(string mensagem)
+        {
+            if (this.InicaArquivo())
+            {
+                FileStream file = new FileStream(nomeArquivo, FileMode.Append);
+                StreamWriter writer = new StreamWriter(file);
+                writer.WriteLine(DateTime.Now.ToString() + ". " + mensagem);
+                writer.Close();
+                file.Close();
+            }
+        }
+    }
+}
